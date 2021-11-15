95989b40ad57999d654ef234778e0837ff68d0d2
Ben Yang <benyang2020@gmail.com>
2021-11-14 23:50:57 -0500

    commit tree ae9b4f1c17ee2074a8a0aa4f000b1158e88d71dd

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+
+  get "/me", to: "users#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  
+  #resources :users, only: [:show]
+  #resources :sessions, only: [:create]
+  #resources :sessions, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
